1564afb054cffd50d7281fdf16d543c9edd9eec9
SijanUnleashed <112467487+sijan-karki@users.noreply.github.com>
2024-05-03 15:25:01 +0545

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp4BySijan.Models;
+
+namespace WebApp4BySijan
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
